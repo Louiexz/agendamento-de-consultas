6bd946a8f6e6c35484698da0fcdee6d81a8ca9e0
Vinicius Silva <137015716+ViniciusRKX@users.noreply.github.com>
2025-04-26 11:27:06 -0300

    MigrationAdd

--- UnitSaude/Migrations/20250426142501_UpdateBanco.Designer.cs
+++ UnitSaude/Migrations/20250426142501_UpdateBanco.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using UnitSaude.Data;
@@ -11,9 +12,11 @@ using UnitSaude.Data;
 namespace UnitSaude.Migrations
 {
     [DbContext(typeof(ClinicaDbContext))]
-    partial class ClinicaDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250426142501_UpdateBanco")]
+    partial class UpdateBanco
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ UnitSaude/Migrations/20250426142501_UpdateBanco.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UnitSaude.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateBanco : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Professores_cpf",
+                table: "Professores",
+                column: "cpf",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Pacientes_cpf",
+                table: "Pacientes",
+                column: "cpf",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Administradores_cpf",
+                table: "Administradores",
+                column: "cpf",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Professores_cpf",
+                table: "Professores");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Pacientes_cpf",
+                table: "Pacientes");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Administradores_cpf",
+                table: "Administradores");
+        }
+    }
+}
