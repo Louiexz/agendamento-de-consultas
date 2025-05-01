4684869af59bb198f18571c127a670262ee903d9
Vinicius Silva <137015716+ViniciusRKX@users.noreply.github.com>
2025-04-30 21:22:10 -0300

    Alteração na estrutura

--- Mobile/app/build.gradle.kts
+++ Mobile/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.unitsaude"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.example.unitsaude"
-        minSdk = 19
-        targetSdk = 35
+        minSdk = 21
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
