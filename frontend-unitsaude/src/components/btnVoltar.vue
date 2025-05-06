ef2609879e4bebbe54c84308c57db7e2b6754ad3
paulo arthur santos <pauloarthurlimadossantos2@gmail.com>
2025-05-06 12:36:15 -0300

    style(btnVoltar): Estilização do botão voltar

--- frontend-unitsaude/src/components/btnVoltar.vue
+++ frontend-unitsaude/src/components/btnVoltar.vue
@@ -28,13 +28,20 @@ export default {
 
 .bi-arrow-left {
   color: #d8bd2c;
-  font-size: 40px;
+  font-size: 35px;
   cursor: pointer;
-  transition: color 0.3s ease;
+  transition: all 0.3s ease;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1); /* sombra leve inicial */
+  border-radius: 100%;
+  padding: 8px;
+  background-color: none;
 }
 
 .bi-arrow-left:hover {
   color: #186fc0;
+  box-shadow: 0 8px 16px rgba(0, 0, 0, 0.2); /* sombra mais forte no hover */
+  transform: translateY(-2px) scale(1.05);  /* leve "pulo" e aumento */
 }
+
 
 </style>
